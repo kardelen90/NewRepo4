a9e91ca85b9e9f2436b612ace01ec4f0d4dd14de
Kardelen Kaya <135968655+kardelen90@users.noreply.github.com>
2025-04-24 14:04:10 +0300

    Add files via upload

---
+++ Sinama_Bufe_Satıs/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinama_Bufe_Satıs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int kasatutar = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int misir, bilet, su, cay, toplam;
+            misir = Convert.ToInt16(TextMısır.Text);
+            bilet = Convert.ToInt16(TextBilet.Text);
+            su = Convert.ToInt16(TextSu.Text);
+            cay = Convert.ToInt16(TextCay.Text);
+
+            toplam = misir * 4 + cay * 2 + su * 1 + bilet * 8;
+            LblToplam.Text = toplam.ToString() + " TL ";
+
+            kasatutar = kasatutar + toplam;
+            LblKasa.Text = kasatutar.ToString() + "  TL ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TextMısır.Text = "";
+            TextBilet.Text = "";
+            TextSu.Text = "";
+            TextCay.Text = "";
+            TextMısır.Focus();
+        }
+    }
+}
---
+++ Sinama_Bufe_Satıs/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sinama_Bufe_Satıs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sinama_Bufe_Satıs")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ecd552a-fd2e-4ffd-81b3-ad2040007f6b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
